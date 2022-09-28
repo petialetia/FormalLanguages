7b3c2fa6306ca49a046eedcf4dd65c8a13854a90
petialeita <petialetia@yandex.ru>
2022-09-28 16:10:27 +0300

    git is changed

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,17 +10,27 @@ int main()
     auto first = nfa.AddState();
     auto second = nfa.AddState();
     auto third = nfa.AddState(true);
+    auto fouth = nfa.AddState(true);
+    auto fifth = nfa.AddState(true);
 
-    nfa.AddTransition({{start, first}, "a"});
-    nfa.AddTransition({{start, second}, "b"});
-    nfa.AddTransition({{first, second}, "b"});
-    nfa.AddTransition({{second, first}, "b"});
-    nfa.AddTransition({{first, third}, "a"});
-    nfa.AddTransition({{second, third}, "a"});
+    nfa.AddTransition({{start, start}, "a"});
+    nfa.AddTransition({{start, first}, "b"});
+    nfa.AddTransition({{first, second}, "a"});
+    nfa.AddTransition({{first, first}, "b"});
+    nfa.AddTransition({{second, start}, "a"});
+    nfa.AddTransition({{second, third}, "b"});
+    nfa.AddTransition({{third, fouth}, "a"});
+    nfa.AddTransition({{third, third}, "b"});
+    nfa.AddTransition({{fouth, fifth}, "a"});
+    nfa.AddTransition({{fouth, third}, "b"});
+    nfa.AddTransition({{fifth, fifth}, "a"});
+    nfa.AddTransition({{fifth, third}, "b"});
+
+    SaveInDOA(nfa, "Start.doa");
 
     ChangeToMinimalDFA(nfa);
 
-    SaveInDOA(nfa);
+    SaveInDOA(nfa, "Changed.doa");
 
     return 0;
 }
